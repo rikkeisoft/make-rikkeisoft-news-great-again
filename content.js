3dee856bb2e6292f35037a191761372890e62294
HuyNQ <huy@huynq.net>
2018-11-16 11:10:18 +0700

    Display custom emoticons in emoticon chooser box

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 const IMAGE_TYPE = 0
 const TEXT_TYPE = 1
-const imgDict = [
+const IMG_TO_EMO_DICT = [
   ['https://rikkei.vn/asset_news/images/emoticons/smile.gif', 'https://i.imgur.com/YGIkOiD.png'],
 ]
-const textDict = [
+const CUSTOM_TEXT_TO_EMO_DICT = [
   [':thuy1:', 'https://i.imgur.com/DaUJ5UR.png'],
   [':thuy2:', 'https://i.imgur.com/l9EkGrs.png'],
   [':thuy3:', 'https://i.imgur.com/D3biQwr.png'],
@@ -11,10 +11,16 @@ const textDict = [
   [':thuy5:', 'https://i.imgur.com/Z9sNSrS.jpg'],
   [':vu1:', 'https://i.imgur.com/O2FHsBc.jpg'],
   [':sa1:', 'https://i.imgur.com/IrN2eyd.jpg'],
+  [':tuan1:', 'https://i.imgur.com/ssTJEGX.jpg'],
+  [':tuan2:', 'https://i.imgur.com/cYKm7QL.jpg'],
+  [':tuan3:', 'https://i.imgur.com/LB5s4YO.jpg'],
+  [':dat1:', 'https://i.imgur.com/t9F5lf2.jpg'],
+]
+const TEXT_TO_EMO_DICT = [
   ['<3', 'https://rikkei.vn/asset_news/images/emoticons/inlove.gif']
 ]
-const imgMap = new Map(imgDict)
-const textMap = new Map(textDict)
+const imgMap = new Map(IMG_TO_EMO_DICT)
+const textMap = new Map([...CUSTOM_TEXT_TO_EMO_DICT, ...TEXT_TO_EMO_DICT])
 
 const getReplacingContent = (value, type = TEXT_TYPE) => {
   let content
@@ -26,7 +32,7 @@ const getReplacingContent = (value, type = TEXT_TYPE) => {
   return content
 }
 
-const clean = () => {
+const decorate = () => {
   const elements = document.querySelectorAll('.span-comment')
 
   for (let i = 0; i < elements.length; i++) {
@@ -47,15 +53,26 @@ const clean = () => {
   }
 }
 
+const addToEmoList = () => {
+  document.querySelector('.emoji-button').click()
+  document.querySelector('body > div.emoji-menu').style.display = 'none'
+  const emoListElement = document.querySelector('body > div.emoji-menu').children[0]
+  for (const item of CUSTOM_TEXT_TO_EMO_DICT) {
+    const newNode = `<a href="javascript:void(0)" title="${item[0]}"><img src="${item[1]}" alt="${item[0]}"><span class="label">${item[0]}</span></a>`
+    emoListElement.innerHTML += newNode
+  }
+}
+
 const callback = (mutationsList, observer) => {
   for (let mutation of mutationsList) {
     if (mutation.type == 'childList') {
-      clean()
+      decorate()
     }
   }
 }
 
-clean()
+decorate()
+addToEmoList()
 
 const targetNode = document.getElementById('list-comment')
 const config = {
