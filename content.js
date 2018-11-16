2b3ad785fe8436d3bf1ef0d7cc171665e00f2245
HuyNQ <huy@huynq.net>
2018-11-16 09:45:19 +0700

    Add v0.0.3

--- content.js
+++ content.js
@@ -1,22 +1,51 @@
-const clean = function() {
-  const elements = document.getElementsByTagName('*')
+const IMAGE_TYPE = 0
+const TEXT_TYPE = 1
+const imgDict = [
+  ['https://rikkei.vn/asset_news/images/emoticons/toan.png', 'https://i.imgur.com/YGIkOiD.png'],
+]
+const textDict = [
+  [':thuy1:', 'https://i.imgur.com/DaUJ5UR.png'],
+  [':thuy2:', 'https://i.imgur.com/l9EkGrs.png'],
+  [':thuy3:', 'https://i.imgur.com/D3biQwr.png'],
+  [':thuy4:', 'https://i.imgur.com/gNBKAvp.png'],
+  [':thuy5:', 'https://i.imgur.com/Z9sNSrS.jpg'],
+  ['<3', 'https://rikkei.vn/asset_news/images/emoticons/inlove.gif']
+]
+const imgMap = new Map(imgDict)
+const textMap = new Map(textDict)
+
+const getReplacingContent = (value, type = TEXT_TYPE) => {
+  let content
+  if (type === TEXT_TYPE) {
+    content = textMap.get(value)
+  } else {
+    content = imgMap.get(value)
+  }
+  return content
+}
+
+const clean = () => {
+  const elements = document.querySelectorAll('.span-comment')
 
   for (let i = 0; i < elements.length; i++) {
-    const element = elements[i]
-
-    for (let j = 0; j < element.childNodes.length; j++) {
-      const node = element.childNodes[j]
-
-      if (node.nodeType === 1) {
-        if (node.src === 'https://rikkei.vn/asset_news/images/emoticons/toan.png') {
-          node.src = 'https://i.imgur.com/YGIkOiD.png'
+    const node = elements[i]
+    if (node.nodeType === 1) {
+      if (node.lastElementChild && node.lastElementChild.tagName === 'IMG') {
+        const newSrc = getReplacingContent(node.lastElementChild.src, IMAGE_TYPE)
+        if (newSrc) {
+          node.lastElementChild.src = newSrc
+        }
+      } else {
+        const newSrc = getReplacingContent(node.innerText, TEXT_TYPE)
+        if (newSrc) {
+          node.innerHTML = `<img src="${newSrc}" class="img-big">`
         }
       }
     }
   }
 }
 
-const callback = function (mutationsList, observer) {
+const callback = (mutationsList, observer) => {
   for (let mutation of mutationsList) {
     if (mutation.type == 'childList') {
       clean()
