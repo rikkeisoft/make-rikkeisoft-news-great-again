cc7808cac9a963336395271fe7d24fc157478701
HuyNQ <huy@huynq.net>
2018-11-21 11:27:20 +0700

    Add v0.1.0

--- js/content.js
+++ js/content.js
@@ -2,6 +2,7 @@ const IMAGE_TYPE = 0
 const TEXT_TYPE = 1
 const IMG_TO_EMO_DICT = [
   ['https://rikkei.vn/asset_news/images/emoticons/smile.gif', 'https://i.imgur.com/YGIkOiD.png'],
+  ['https://rikkei.vn/asset_news/images/emoticons/rainbowsmile.gif', 'https://i.imgur.com/YGIkOiD.png'],
 ]
 const CUSTOM_TEXT_TO_EMO_DICT = [
   [':thuy1:', 'https://i.imgur.com/DaUJ5UR.png'],
@@ -15,6 +16,43 @@ const CUSTOM_TEXT_TO_EMO_DICT = [
   [':tuan2:', 'https://i.imgur.com/cYKm7QL.jpg'],
   [':tuan3:', 'https://i.imgur.com/LB5s4YO.jpg'],
   [':dat1:', 'https://i.imgur.com/t9F5lf2.jpg'],
+  [':tho1:', 'https://i.imgur.com/ow6D28e.gif'],
+  [':tho2:', 'https://i.imgur.com/vp2m2q0.gif'],
+  [':tho3:', 'https://i.imgur.com/pzWRzHc.gif'],
+  [':tho4:', 'https://i.imgur.com/akb1HaS.gif'],
+  [':tho5:', 'https://i.imgur.com/RUcyMv7.gif'],
+  [':tho6:', 'https://i.imgur.com/DKAIcE9.gif'],
+  [':tho7:', 'https://i.imgur.com/JNg85C4.gif'],
+  [':tho8:', 'https://i.imgur.com/fDtmSjC.gif'],
+  [':tho9:', 'https://i.imgur.com/GisUWG1.gif'],
+  [':tho10:', 'https://i.imgur.com/4itFrVA.gif'],
+  [':tho11:', 'https://i.imgur.com/NTofTXn.gif'],
+  [':tho12:', 'https://i.imgur.com/ZV510FB.gif'],
+  [':tho13:', 'https://i.imgur.com/tQUCSw0.gif'],
+  [':tho14:', 'https://i.imgur.com/CW7W2hm.gif'],
+  [':tho15:', 'https://i.imgur.com/Q0zdF5B.gif'],
+  [':tho16:', 'https://i.imgur.com/3o7gfE6.gif'],
+  [':tho17:', 'https://i.imgur.com/3o7gfE6.gif'],
+  [':tho18:', 'https://i.imgur.com/lzEZpGO.gif'],
+  [':tho19:', 'https://i.imgur.com/NLkRB1J.gif'],
+  [':tho20:', 'https://i.imgur.com/AvDeOSO.gif'],
+  [':tho21:', 'https://i.imgur.com/pxGZ5B8.gif'],
+  [':bored_parrot:', 'https://i.imgur.com/U4hWizq.gif'],
+  [':bow:', 'https://i.imgur.com/JwnKJsc.gif'],
+  [':coffee_parrot:', 'https://i.imgur.com/5EaIwM9.gif'],
+  [':doge_bread:', 'https://i.imgur.com/eDetip4.jpg'],
+  [':doge:', 'https://i.imgur.com/39eQNsx.jpg'],
+  [':slide_parrot:', 'https://i.imgur.com/JFg0DsX.gif'],
+  [':fast_parrot:', 'https://i.imgur.com/aN0qzVH.gif'],
+  [':grumpy:', 'https://i.imgur.com/eOfyX7n.png'],
+  [':mooning:', 'https://i.imgur.com/MB28a06.png'],
+  [':nyan_cat:', 'https://i.imgur.com/XXNirx9.png'],
+  [':panda:', 'https://i.imgur.com/Dzy9Pqb.gif'],
+  [':parrot:', 'https://i.imgur.com/0utmyvj.gif'],
+  [':reverse_parrot:', 'https://i.imgur.com/kmw04qg.gif'],
+  [':ship_parrot:', 'https://i.imgur.com/iijidYX.gif'],
+  [':stop:', 'https://i.imgur.com/U1b6GOk.gif'],
+  [':slow_parrot:', 'https://i.imgur.com/nkbPduW.gif'],
 ]
 const TEXT_TO_EMO_DICT = [
   ['<3', 'https://rikkei.vn/asset_news/images/emoticons/inlove.gif']
@@ -87,14 +125,18 @@ if (isRoot) {
   if (preferNoSidebar) {
     const sidebarElement = document.querySelector('.blog-sidebar-wrapper')
     const blogContentElement = document.querySelector('.blog-content')
+    const ribbonElements = document.querySelectorAll('.thumbnail img.new-icon')
     if (sidebarElement) {
       sidebarElement.remove()
     }
     if (blogContentElement) {
       blogContentElement.className = 'blog-content'
     }
+    ribbonElements.forEach(element => {
+      element.style.display = 'none'
+    })
   }
-} else if (isPost) {  
+} else if (isPost) {
   if (preferNoSidebar) {
     const sidebarElement = document.querySelector('.blog-sidebar-wrapper')
     const blogContentElement = document.querySelector('.blog-content')
