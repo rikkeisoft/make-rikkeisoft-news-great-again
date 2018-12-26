fa5d8139a8c44bea2eb39a797b43ce2ddee87610
HuyNQ <huy@huynq.net>
2018-12-26 16:26:49 +0700

    Bump to 0.2.7

--- js/content.js
+++ js/content.js
@@ -136,11 +136,13 @@ const decorateMentions = () => {
           if (person) {
             CACHED_PROFILE[person.email] = person
             const userTeam = person.team.split('-').map(team => teamData[team].name).join(' / ')
+            const userAvatar = person.avatar_url ? person.avatar_url.replace('?sz=50', '') : DEFAULT_AVATAR
             const popoverContent = document.createElement('div')
             popoverContent.className = 'push popover__content'
+            
             const profileHTML = `
 <div class="profile">
-  <div class="profile-photo"><img src="${person.avatar_url || DEFAULT_AVATAR}" /></div>
+  <div class="profile-photo"><a href="${url.href}" target="_blank"><img src="${userAvatar}" /></a></div>
   <div class="profile-content">
     <div class="profile-text">
       <h4>${person.name}</h4>
@@ -148,8 +150,6 @@ const decorateMentions = () => {
       <h5><i class="fa fa-user-o"></i>${person.employee_code}</h6>
       <h5><i class="fa fa-birthday-cake color-birth"></i>${person.birthday}</h6>
       <h5><i class="fa fa-users color-team"></i>${userTeam}</h6>
-      <h5><i class="fa fa-phone color-phone"></i>${person.mobile_phone}</h6>
-      <h5><i class="fa fa-skype color-skype"></i>${person.skype}</h6>
     </div>
   </div>
 </div>
